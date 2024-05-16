b6a7394c9edf6bc6e162d14ac320c08a53c55b2a
Palloma <nunespalloma@outlook.com>
2024-05-16 13:09:02 -0300

    Implementações do desafio e mudança da versão do .NET para a 8.0

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "123", modelo: "Modelo teste", imei: "112233", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("LinkedIn");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero: "123", modelo: "Modelo teste", imei: "112233", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("LinkedIn");
